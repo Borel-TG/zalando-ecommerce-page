9a8759a8385a7ab747b5bdd7a335a4369d0d1704
Borel-TG <tchassemborel@gmail.com>
2025-01-17 19:50:48 +0300

    feat : added custom carousel component

---
+++ src/ui/components/carousel/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React, { useRef } from "react";
+import { Button } from "antd";
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function ProductCarousel(props: Props) {
+  const carouselRef = useRef<HTMLDivElement>(null);
+
+  const scrollLeft = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({
+        left: -300,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({
+        left: 300,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <div className="relative">
+      <Button
+        icon={<LeftOutlined />}
+        className="absolute left-2 !rounded-none border !border-black top-[150px] transform -translate-y-1/2 z-10"
+        onClick={scrollLeft}
+      />
+      <Button
+        icon={<RightOutlined />}
+        className="absolute !rounded-none border !border-black top-[150px] left-[calc(100%-70px)] md:left-[calc(100%-50px)]  transform -translate-y-1/2 z-10"
+        onClick={scrollRight}
+      />
+      <div
+        className="flex space-x-4 overflow-x-auto scrollbar-hide"
+        ref={carouselRef}
+      >
+        {props.children}
+      </div>
+    </div>
+  );
+}
